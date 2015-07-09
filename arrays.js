df5bcc3572fc1ba159520f745c870205432762ce
Craig Franklin <craigmfranklin@gmail.com>
2015-07-09 16:57:52 -0400

    First commit

---
+++ arrays.js
@@ -0,0 +1,14 @@
+// Exercises: Arrays
+
+// EXERCISE: Your top choices
+
+// Create an array to hold your top choices (colors, presidents, whatever).
+// For each choice, log to the screen a string like: "My #1 choice is blue."
+// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+
+var myHeroes = ["Batman", "Wolverine", "Daredevil"];
+
+document.write("My #1 superhero is " + myHeroes[0] + " my #2 superhero is " + myHeroes[1] + " and my #3 superhero is " + myHeroes[2] + "<br>");
+
+
+
